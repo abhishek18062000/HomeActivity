b26410c73f25729d64fc328dbe3819abc6a56d59
abhis <abhishekmaurya18062000@gmail.com>
2020-11-10 17:04:20 +0530

    updated register layout activity added

--- app/src/main/java/com/example/homeactivity/Home/MainActivity.java
+++ app/src/main/java/com/example/homeactivity/Home/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.homeactivity.Home;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +20,10 @@ import android.view.View;
 import com.example.homeactivity.Login.LoginActivity;
 import com.example.homeactivity.R;
 import com.example.homeactivity.utils.BottomNavigationViewHelper;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
@@ -22,26 +31,64 @@ import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "HomeActivity";
-    private Context mcontext= MainActivity.this;
-    private static final int ACTIVITY_NUM =0;
+    private Context mcontext = MainActivity.this;
+    private static final int ACTIVITY_NUM = 0;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListner;
 
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        getCurrentLocation();
         setupFirebaseAuth();
         setupBottomNavigationView();
         setViewPager();
     }
+
+    private void getCurrentLocation() {
+        final LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setPriority(locationRequest.PRIORITY_HIGH_ACCURACY);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.getFusedLocationProviderClient(MainActivity.this).requestLocationUpdates(locationRequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                LocationServices.getFusedLocationProviderClient(MainActivity.this).removeLocationUpdates(this);
+                if(locationResult !=null && locationResult.getLocations().size()>0){
+                    int latestLocationIndex=locationResult.getLocations().size()-1;
+                    double latitude=locationResult.getLocations().get(latestLocationIndex).getLatitude();
+                    double longitude=locationResult.getLocations().get(latestLocationIndex).getLongitude();
+                    Log.d(TAG, "LATITUDE:----------------------------------------------------"+latitude);
+                    Log.d(TAG, "LONGITUDE:--------------------------------------------------"+longitude);
+                }
+            }
+        }, Looper.getMainLooper());
+
+   }
+
     private void checkCurrentUser(FirebaseUser user){
         if (user == null){
             Intent intent= new Intent(mcontext, LoginActivity.class);
             startActivity(intent);
         }
     }
+
     private void setupFirebaseAuth(){
         mAuth = FirebaseAuth.getInstance();
         mAuthListner= new FirebaseAuth.AuthStateListener() {
@@ -93,4 +140,8 @@ public class MainActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
     }
+
+
+
+
 }
